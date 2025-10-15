45e399bb4d3d994aa625f6c20885496d6ab7abcf
almaz <almazbanker@gmail.com>
2025-10-15 22:34:41 +0600

    hw python week-4

--- python/week-1/hw.py
+++ python/week-1/hw.py
@@ -1,12 +1,25 @@
-print('Enter your name:')
-name = input()
+print('How many people are splitting the bill:')
+countPeople = int(input())
+
+people = []
+
+for i in range(1, countPeople + 1):
+    print(f'Enter a name for the {i} person')
+    people.append(input().capitalize())
+
 print('Enter the total bill amount:')
 totalBillAmount = int(input())
 print('Enter the tip percentage:')
 tipPercentage= int(input())
 
-tip = (totalBillAmount/100) * tipPercentage
-totalAmount = totalBillAmount + tip
+tip = round((totalBillAmount/100) * tipPercentage, 2)
+totalAmount = round(totalBillAmount + tip, 2)
 
-print(name + ', Total tip: ' + str(tip))
-print(name + ', Total amount: ' + str(totalAmount))
+everyPerson = round(totalAmount/countPeople, 2)
+
+print(f'Total tip: {tip}')
+print(f'Total amount: {totalAmount}')
+print(f'Every person must pay {everyPerson}')
+
+if tipPercentage > 20:
+    print('Thank you for your generosity!')
